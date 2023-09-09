331df5bc035ff03210d843fbcdcbe666b6b8b751
Iván <ivanflowtramites@gmail.com>
2023-09-09 18:46:06 -0300

    Initial commit

---
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -0,0 +1,11 @@
+// Learning Progress Tracker (Java / Java Desktop Application Developer) -
+// Project Completed By Iván Luna, September 8, 2023. -
+// For Hyperskill (Jet Brains Academy). Course: Java Desktop Application Developer.
+
+package tracker;
+
+public class Main {
+    public static void main(String[] args) {
+        new UserInteraction();
+    }
+}
